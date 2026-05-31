791675027004cf7f53b64972ce417fc06c7121fc
sjaykumar7272 <sjaykumar7272@gmail.com>
2026-05-31 23:04:53 +0530

    final touch 2

--- src/components/dialog_share_subject.py
+++ src/components/dialog_share_subject.py
@@ -6,7 +6,7 @@ import io
 
 @st.dialog("Share Class Link")
 def share_subject_dialog(subject_name, subject_code):
-    app_domain = "Making-Attendance-faster-using-AI.streamlit.app"
+    app_domain = "making-attendance-faster-using-ai.streamlit.app"
     join_url = f"{app_domain}/?join-code={subject_code}"
 
     st.header("Scan to Join")
