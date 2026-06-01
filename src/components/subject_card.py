981ccdf0262651629c35505e83a001ec23f32e87
sjaykumar7272 <sjaykumar7272@gmail.com>
2026-06-01 14:28:31 +0530

    fixed some subject_card

--- src/components/subject_card.py
+++ src/components/subject_card.py
@@ -1,7 +1,7 @@
 import streamlit as st
 def subject_card(name, code, section, stats=None, footer_callback=None):
     html = f"""
-        <div style="background:white; border-left: 8px solid #EB459E; padding:25px; border-radius: 20px; border: 1px solid black; margin-bottom:20px;">
+        <div style="background:#9cb8e8; border-left: 8px solid #EB459E; padding:25px; border-radius: 20px; border: 1px solid black; margin-bottom:20px;">
         <h3 style="margin:0; color: #1e293b; font-size: 1.5rem ">{name}</h3>
         <p style="color:#64748b; margin:10px 0;">Code : <span style="background:#E0E3FF; color:#5865F2; padding:2px 8px; border-radius:5px;">{code} </span> | Section : {section}</p>
         
